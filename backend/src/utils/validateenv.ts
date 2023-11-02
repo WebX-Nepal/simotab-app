411c0c13a57b95752bd6763ae10630ce3af81f38
prabash19 <prabash19@gmail.com>
2023-11-02 11:58:52 +0545

    merge for prod

--- backend/src/utils/validateenv.ts
+++ backend/src/utils/validateenv.ts
@@ -1,5 +1,10 @@
 import { cleanEnv, str, port } from "envalid";
-import "dotenv/config";
+import dotenv from "dotenv";
+import path from "path";
+
+const envFilePath = path.resolve(path.dirname(__dirname), "../.env");
+// Construct the path to the .env file in the root directory
+dotenv.config({ path: envFilePath });
 export default cleanEnv(process.env, {
   MONGO_URL: str(),
   PORT: port(),
