f3f6de2691e0c892e11f5224d7ee30c4d80b9eac
prabash19 <prabash19@gmail.com>
2023-11-02 17:24:07 +0545

    changes

--- backend/src/app/index.ts
+++ backend/src/app/index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
 import passport from "passport";
 import cookieParser from "cookie-parser";
 import mongoSanitize from "express-mongo-sanitize";
@@ -21,20 +20,8 @@ connectdb();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
-const allowedOrigins = ["https://simotap.com", "http://localhost:5173/"];
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  next();
-});
-const whitelist = ["https://simotap.com/"];
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-};
-app.use(cors(corsOptions));
+
+
 
 app.use(mongoSanitize());
 app.use(compression());
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,9 @@
 import { app } from "./app/index";
 import env from "./utils/validateenv";
+import cors from "cors";
 
-
-
-app.listen(env.PORT,()=>{
-    console.log(` server is running running  ${env.PORT}`)
-})
+app.listen(env.PORT, () => {
+  console.log(` server is running running  ${env.PORT}`);
+});
+app.use(cors());
+app.options("*", cors());
