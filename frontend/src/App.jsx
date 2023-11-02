6280472941774774ee3997f3f1d8aa196575e6ee
prabash19 <prabash19@gmail.com>
2023-11-02 12:04:49 +0545

    frontend particle package changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css"
+import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import SignUp from "./pages/signup";
 import SignIn from "./pages/signin";
@@ -15,43 +15,39 @@ import EditProfile from "./pages/admin/My-Profile/Edit-Profile";
 import HomeSection from "./pages/Home/HomeSection";
 import UpdateContact from "./components/forms/Contact/updateContact";
 import UpdateLink from "./components/forms/Link/updateLink";
-import CreateContact from './components/forms/Contact/createContact'
+import CreateContact from "./components/forms/Contact/createContact";
 import CreateLink from "./components/forms/Link/createLink";
 import SecureRoute from "./secure_routes/login_user_only";
 import Setting from "./pages/admin/Setting";
-import Particles from "react-tsparticles";
+import Particles from "react-particles";
 import { loadFull } from "tsparticles";
 import particleConfig from "./components/particleBg/particleConfig";
-import { useCallback, useState, useEffect, useRef } from 'react';
-
-
+import { useCallback, useState, useEffect, useRef } from "react";
 
 function App() {
-
-
-  const particlesInit = useCallback(async engine => {
+  const particlesInit = useCallback(async (engine) => {
     await loadFull(engine);
   }, []);
 
-  const particlesLoaded = useCallback(async container => {
+  const particlesLoaded = useCallback(async (container) => {
     // await console.log(container);
   }, []);
   const [maskStyle, setMaskStyle] = useState({});
   const [isHovering, setIsHovering] = useState(false);
-  const [isScrolled, setIsScrolled] = useState(false)
+  const [isScrolled, setIsScrolled] = useState(false);
 
   const handleMouseMove = (e) => {
     let x, y;
 
-    if (e.type === 'mousemove' || e.type === 'touchmove') {
+    if (e.type === "mousemove" || e.type === "touchmove") {
       x = e.clientX;
       y = e.clientY;
-    } else if (e.type === 'touchmove' && e.touches.length > 0) {
+    } else if (e.type === "touchmove" && e.touches.length > 0) {
       x = e.touches[0].pageX;
       y = e.touches[0].pageY;
     }
     setMaskStyle({
-      position: 'fixed',
+      position: "fixed",
       background: `radial-gradient(circle 150px at ${x}px ${y}px, transparent 50%, #EDEDED 100%)`,
     });
   };
@@ -64,12 +60,13 @@ function App() {
     setIsHovering(false);
   };
   const scrollToTop = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
   // Function to handle showing/hiding the scroll-to-top button
   const handleScroll = () => {
-    if (window.scrollY > 200) { // You can adjust the threshold here
+    if (window.scrollY > 200) {
+      // You can adjust the threshold here
       setIsScrolled(true);
     } else {
       setIsScrolled(false);
@@ -78,35 +75,27 @@ function App() {
 
   // Attach the scroll event listener when the component mounts
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
 
     // Remove the event listener when the component unmounts
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
-
 
   return (
     <>
       <div
-        className={`container ${isHovering ? 'hovering' : ''}`}
+        className={`container ${isHovering ? "hovering" : ""}`}
         onMouseMove={handleMouseMove}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
       >
-
         <div className="content-container">
-
-
-
           <Routes>
-
             <Route path="/" element={<Navbar />}>
               <Route path="/" element={<HomeSection />} />
               <Route path="/shop" element={<Shop />} />
-
-
 
               <Route path="/shop/:id" element={<Single_Cart_Review />} />
 
@@ -118,14 +107,15 @@ function App() {
 
             {/* admin routes */}
 
-
             <Route path="/admin" element={<SecureRoute />}>
-
               <Route path="/admin/my-profile" element={<MyProfile />} />
               <Route path="/admin/my-cards" element={<MyCards />} />
               <Route path="/admin/contacts" element={<Contact />} />
               <Route path="/admin/contacts/:id" element={<UpdateContact />} />
-              <Route path="/admin/create-contact-form" element={<CreateContact />} />
+              <Route
+                path="/admin/create-contact-form"
+                element={<CreateContact />}
+              />
               <Route path="/admin/edit-profile" element={<EditProfile />} />
               <Route path="/admin/edit-profile/:id" element={<UpdateLink />} />
               <Route path="/admin/create-link" element={<CreateLink />} />
@@ -134,27 +124,22 @@ function App() {
               <Route path="/admin/buy-simotap" element={<Setting />} />
               <Route path="/admin/Booking" element={<Setting />} />
               <Route path="/admin/social-media-kits" element={<Setting />} />
-
             </Route>
 
-
-
             <Route path="*" element={<UnMatchedRoutePage />} />
-
           </Routes>
         </div>
 
-
-        <div className={`particle-div ${isHovering ? '' : 'hidden'}`}><Particles
-          id="tsparticles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={particleConfig}
-        /></div>
+        <div className={`particle-div ${isHovering ? "" : "hidden"}`}>
+          <Particles
+            id="tsparticles"
+            init={particlesInit}
+            loaded={particlesLoaded}
+            options={particleConfig}
+          />
+        </div>
         <div className="hover-div" style={maskStyle}></div>
-      </div >
-
-
+      </div>
     </>
   );
 }
